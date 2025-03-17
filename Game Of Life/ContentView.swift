72c37ef42b8684e2a72f5ef4bcc4bbec134a1d91
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-17 20:08:59 +0300

    Initial Commit

---
+++ Game Of Life/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Game Of Life
+//
+//  Created by mehmet Çelik on 17.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Game Of Life/Game_Of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Of_LifeApp.swift
+//  Game Of Life
+//
+//  Created by mehmet Çelik on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Game_Of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
