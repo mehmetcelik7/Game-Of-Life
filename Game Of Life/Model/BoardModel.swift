27f1a0090b6bb12547f373508969782851ba66a6
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-19 09:18:23 +0300

    1

--- Game Of Life/Model/BoardModel.swift
+++ Game Of Life/Model/BoardModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct BoardModel {
+    var gridSize: Int
+    var creatures: [[Int]] = []
+    
+    init(gridSize: Int) {
+        self.gridSize = gridSize
+        self.randomBoard()
+    }
+    
+    mutating func randomBoard() {
+        creatures = []
+        
+        for _ in 0..<gridSize {
+            creatures
+                .append(
+                    Array(repeating: 0, count: gridSize)
+                        .map{ _ in
+                            Int.random(in: 0...1)
+                        }
+                )
+        }
+    }
+    
+    
+    mutating func clearBoard() {
+        creatures = Array(repeating: Array(repeating: 0, count: gridSize), count: gridSize)
+    }
+    
+    func countLiveNeighbours(row: Int, col: Int) -> Int {
+        let directions: [(x:Int,y:Int)] = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
+        var count = 0
+        
+        for direction in directions {
+            let newRow = (row + direction.x + gridSize) % gridSize
+            let newCol = (col + direction.y + gridSize) % gridSize
+            count += self.creatures[newRow][newCol]
+        }
+        
+        
+        return count
+    }
+    
+}
--- Game Of Life/View/Image Resources/BackgroundImages.swift
+++ Game Of Life/View/Image Resources/BackgroundImages.swift
@@ -5,4 +5,23 @@
 //  Created by mehmet Çelik on 17.03.2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct BackgroundImages {
+    static let all: [ImageResource] = [
+        .autumn1,
+        .autumn2,
+        .autumn3,
+        .mountain1,
+        .mountain2,
+        .mountain3,
+        .space1,
+        .space2,
+        .underwater1,
+        .underwater2,
+        .underwater3,
+        .cyber1,
+        .cyber2,
+        .code1
+    ]
+}
