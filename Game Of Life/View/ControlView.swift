237888f04800673a11e16f840ad1448f794c58ad
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-20 16:06:01 +0300

    9

--- Game Of Life/View/ControlView.swift
+++ Game Of Life/View/ControlView.swift
@@ -30,6 +30,34 @@ struct ControlView : View {
     }
 }
 
+enum ControlButtonType {
+    case playPause(isPlaying : Bool)
+    case refresh, clear, toggleImage, changeImage, toggleVisibility(isHidden: Bool)
+    
+    var systemImage: String {
+        switch self {
+            
+        case .playPause(isPlaying: let isPlaying):
+            isPlaying ? "pause" : "play"
+        case .refresh:
+            "arrow.counterclockwise"
+        case .clear:
+            "clear"
+        case .toggleImage:
+            "photo.on.rectangle"
+        case .changeImage:
+            "arrow.triangle.2.circlepath"
+        case .toggleVisibility(isHidden: let isHidden):
+            isHidden ? "laydbug" : "ladybug.slash"
+        }
+    }
+    
+    
+}
+
+
+
+
 struct ButtonView: View {
     let name: String
     let font: Font = Font.headline
